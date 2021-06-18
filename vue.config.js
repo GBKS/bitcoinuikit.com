14bd851e5d83b3760430b4dc4f84767fa93dcfdb
GBKS <chri@sto.ph>
2021-06-18 09:50:45 +0200

    Title update

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
             .plugin('html')
             .tap(args => {
                 args[0].title = "Bitcoin UI Kit";
-                args[0].description = "Interface icons for Bitcoin applications, free to use.";
+                args[0].description = "A design system and UI kit for Bitcoin wallet applications.";
                 args[0].locale = "en_US";
                 args[0].canonical = "https://bitcoinuikit.com";
                 args[0].preview = "https://bitcoinuikit.com/bitcoin-ui-kit.png";
