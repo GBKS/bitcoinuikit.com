901d8dfc4e23712813d3373c44edb79479eba749
GBKS <chri@sto.ph>
2021-06-18 10:03:17 +0200

    Preview image update

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
                 args[0].description = "A design system and UI kit for Bitcoin wallet applications.";
                 args[0].locale = "en_US";
                 args[0].canonical = "https://bitcoinuikit.com";
-                args[0].preview = "https://bitcoinuikit.com/bitcoin-ui-kit.png";
+                args[0].preview = "https://bitcoinuikit.com/bitcoin-ui-kit.jpg";
                 return args;
             })
     }
