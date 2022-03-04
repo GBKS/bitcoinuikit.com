1372c7f871b99d363be25d586ae8c29bfb67c22b
GBKS <chri@sto.ph>
2022-03-04 09:25:23 +0100

    Added reference to Amans RN library

--- src/components/SiteInfo.vue
+++ src/components/SiteInfo.vue
@@ -27,8 +27,6 @@
       <li>Developers can pick elements from the UI kit to include in their&nbsp;applications</li>
     </ul>
 
-    <p>Also check out the <a href="https://github.com/reez/WalletUI" target="_blank" rel="nofollow noreferrer notarget">WalletUI project</a>. reez is turning this kit into an iOS&nbsp;library.</p>
-
     <h2 id="contribute">How to contribute</h2>
 
     <p>The kit is a big work-in-progress and you are welcome to chip in. It is primarily maintained by <a href="https://twitter.com/gbks" target="_blank" rel="nofollow noreferrer notarget">GBKS</a>. If you have feedback, leave a comment directly in the Figma file or <a href="https://github.com/GBKS/bitcoin-wallet-ui-kit" target="_blank" rel="me">Github repo</a>.</p>
@@ -44,6 +42,13 @@
     </ul>
 
     <p>To propose specific design changes, duplicate the file, make your changes, and leave a comment on the original file. We can then together review and merge the changes into the main file. You can also publish your file as a&nbsp;<a href="https://help.figma.com/hc/en-us/articles/360038510693-Guide-to-Figma-Community#Community_files" target="_blank" rel="nofollow noreferrer notarget">Remix</a>.</p>
+
+    <p>You can also help out with code implementations:</p>
+    
+    <ul>
+      <li><a href="https://github.com/reez/WalletUI" target="_blank" rel="nofollow noreferrer notarget">iOS library</a> by <a href="https://github.com/reez" target="_blank" rel="nofollow noreferrer notarget">reez</a></li>
+      <li><a href="https://github.com/Aman035/wallet-ui-react" target="_blank" rel="nofollow noreferrer notarget">React Native library</a> by <a href="https://github.com/Aman035" target="_blank" rel="nofollow noreferrer notarget">Aman035</a> (currently unmaintained)</li>
+    </ul>
 
     <h2 id="license">License</h2>
 
@@ -111,6 +116,10 @@ export default {
 
     li {
 
+    }
+
+    & + h2 {
+      margin-top: 60px;
     }
   }
 
